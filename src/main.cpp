e407190b3dfa107c1d811a2ff69100dae4bd1687
shendricks <shendricks@bensonhill.com>
2023-08-28 14:48:53 -0500

    wind speed an direction

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,15 @@
-#include <Arduino.h>
 
-// put function declarations here:!!
-int myFunction(int, int);
+#include "Arduino.h"
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+void setup()
+{
+    Serial.begin(9600);
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+    Serial.println("Hello world!");
+    delay(1000);
 }
